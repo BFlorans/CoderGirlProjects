b7b158f361ef4c56d3cb1cfccdd6fa68527a1f57
BFlorans <40842753+BFlorans@users.noreply.github.com>
2018-09-14 14:06:58 -0500

    Update TemperaturesMultiDArrays.java

--- TemperaturesMultiDArrays.java
+++ TemperaturesMultiDArrays.java
@@ -2,35 +2,12 @@ package com.company;
 import static java.lang.System.out;
 public class TemperaturesMultiDArrays {
     public static void main(String[] args){
-        int temperatures[][] = new int[4][7];
-        temperatures[0][0] = 68;
-        temperatures[0][1] = 70;
-        temperatures[0][2] = 76;
-        temperatures[0][3] = 70;
-        temperatures[0][4] = 68;
-        temperatures[0][5] = 71;
-        temperatures[0][6] = 75;
-        temperatures[1][0] = 76;
-        temperatures[1][1] = 76;
-        temperatures[1][2] = 87;
-        temperatures[1][3] = 84;
-        temperatures[1][4] = 82;
-        temperatures[1][5] = 75;
-        temperatures[1][6] = 83;
-        temperatures[2][0] = 73;
-        temperatures[2][1] = 72;
-        temperatures[2][2] = 81;
-        temperatures[2][3] = 78;
-        temperatures[2][4] = 76;
-        temperatures[2][5] = 73;
-        temperatures[2][6] = 77;
-        temperatures[3][0] = 64;
-        temperatures[3][1] = 65;
-        temperatures[3][2] = 69;
-        temperatures[3][3] = 68;
-        temperatures[3][4] = 70;
-        temperatures[3][5] = 74;
-        temperatures[3][6] = 72;
+        int temperatures[][] = {
+                {68, 70, 76, 70, 68, 71, 75},
+                {76, 76, 87, 84, 82, 75, 83},
+                {73, 72, 81, 78, 76, 73, 77},
+                {64, 65, 69, 68, 70, 74, 72}
+        };
 
         String[] days = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
         String[] times = {"7 AM", "3 PM", "7 PM", "3 AM"};
